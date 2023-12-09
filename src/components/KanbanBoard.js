0ad9e51321547cd1b2397c88461ea4f3296fafe4
aashayk18 <aashayk100@gmail.com>
2023-12-09 20:15:06 +0530

    modifications

--- src/components/KanbanBoard.js
+++ src/components/KanbanBoard.js
@@ -3,20 +3,17 @@ import CardList from './CardList'; // Import the CardList component
 import './KanbanBoard.css'; // Import associated CSS file for styling
 
 const KanbanBoard = () => {
-  // State variables
   const [groupingOption, setGroupingOption] = useState('status');
   const [sortBy, setSortBy] = useState('priority');
-  const [tickets, setTickets] = useState([]); // State for fetched ticket data
+  const [tickets, setTickets] = useState([]);
 
-  // Fetch data from the API
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch('https://api.quicksell.co/v1/internal/frontend-assignment');
         const data = await response.json();
-        // Assuming the API response returns an object with 'tickets' and 'users' arrays
-        const { tickets: fetchedTickets } = data; // Extract 'tickets' array from the data object
-        setTickets(fetchedTickets); // Set 'tickets' state with the extracted 'tickets' array
+        // Assuming the API response returns an object with 'tickets' array
+        setTickets(data.tickets || []);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -25,65 +22,122 @@ const KanbanBoard = () => {
     fetchData();
   }, []);
 
-  // Function to handle grouping and sorting logic
   const groupAndSortTickets = () => {
-    let organizedTickets = [...tickets]; // Create a copy of tickets array for manipulation
+    let organizedTickets = [...tickets];
 
-    // Grouping tickets based on 'groupingOption'
     if (groupingOption === 'status') {
-      organizedTickets.sort((a, b) => a.status.localeCompare(b.status));
+      // Group by status logic
+      const statusGroups = {};
+      organizedTickets.forEach(ticket => {
+        if (!statusGroups[ticket.status]) {
+          statusGroups[ticket.status] = [];
+        }
+        statusGroups[ticket.status].push(ticket);
+      });
+
+      // Convert object back to array
+      organizedTickets = Object.values(statusGroups).flat();
     } else if (groupingOption === 'user') {
-      organizedTickets.sort((a, b) => a.userId - b.userId);
+      // Group by user logic
+      const userGroups = {};
+      organizedTickets.forEach(ticket => {
+        if (!userGroups[ticket.userId]) {
+          userGroups[ticket.userId] = [];
+        }
+        userGroups[ticket.userId].push(ticket);
+      });
+
+      // Convert object back to array
+      organizedTickets = Object.values(userGroups).flat();
     } else if (groupingOption === 'priority') {
+      // Group by priority logic
       organizedTickets.sort((a, b) => b.priority - a.priority);
     }
 
-    // Sorting tickets based on 'sortBy'
+    // Sorting based on 'sortBy'
     if (sortBy === 'priority') {
       organizedTickets.sort((a, b) => b.priority - a.priority);
     } else if (sortBy === 'title') {
       organizedTickets.sort((a, b) => a.title.localeCompare(b.title));
     }
 
-    // Update the 'tickets' state with the organized data
     setTickets(organizedTickets);
   };
 
-  // Function to handle user interactions (e.g., changing grouping or sorting)
-  const handleGroupingChange = (option) => {
-    setGroupingOption(option);
-  };
 
-  const handleSortChange = (option) => {
-    setSortBy(option);
-  };
-
-  // Run grouping and sorting logic whenever 'groupingOption' or 'sortBy' changes
   useEffect(() => {
     groupAndSortTickets();
   }, [groupingOption, sortBy]);
 
-  console.log(tickets); 
-  console.log(typeof tickets);
+  const renderColumnsByGroup = () => {
+    let columns = [];
+
+    if (groupingOption === 'status') {
+      const statusGroups = {};
+      tickets.forEach(ticket => {
+        if (!statusGroups[ticket.status]) {
+          statusGroups[ticket.status] = [];
+        }
+        statusGroups[ticket.status].push(ticket);
+      });
+
+      columns = Object.entries(statusGroups).map(([status, cards]) => (
+        <div key={status} className="column">
+          <h3>{status}</h3>
+          <CardList title={status} cards={cards} />
+        </div>
+      ));
+    } else if (groupingOption === 'user') {
+      const userGroups = {};
+      tickets.forEach(ticket => {
+        if (!userGroups[ticket.userId]) {
+          userGroups[ticket.userId] = [];
+        }
+        userGroups[ticket.userId].push(ticket);
+      });
+
+      columns = Object.entries(userGroups).map(([userId, cards]) => (
+        <div key={userId} className="column">
+          <h3>User {userId}</h3>
+          <CardList title={`User ${userId}`} cards={cards} />
+        </div>
+      ));
+    } else if (groupingOption === 'priority') {
+      for (let i = 4; i >= 0; i--) {
+        const priorityLevel = i === 0 ? 'No Priority' : `Priority ${i}`;
+        const priorityCards = tickets.filter(ticket => ticket.priority === i);
+        
+        if (priorityCards.length > 0) {
+          columns.push(
+            <div key={priorityLevel} className="column">
+              <h3>{priorityLevel}</h3>
+              <CardList title={priorityLevel} cards={priorityCards} />
+            </div>
+          );
+        }
+      }
+    }
+
+    return columns;
+  };
 
   return (
     <div className="kanban-board">
-      {/* Example UI components for options */}
       <div className="options">
-        <select value={groupingOption} onChange={(e) => handleGroupingChange(e.target.value)}>
+        <select value={groupingOption} onChange={(e) => setGroupingOption(e.target.value)}>
           <option value="status">By Status</option>
           <option value="user">By User</option>
           <option value="priority">By Priority</option>
         </select>
-        <select value={sortBy} onChange={(e) => handleSortChange(e.target.value)}>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
           <option value="priority">Sort by Priority</option>
           <option value="title">Sort by Title</option>
         </select>
       </div>
 
-      {/* Render CardLists based on grouping */}
-      <CardList title="To Do" cards={tickets} />
-      {/* Other CardList components based on different categories */}
+      <div className="columns-container">
+        {renderColumnsByGroup()}
+      </div>
     </div>
   );
 };
